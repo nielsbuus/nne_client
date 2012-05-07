7d48471b821b749e1f87205d9928d9c2e6ae2036
Jacob Atzen <jacob@incremental.dk>
2012-05-07 17:17:28 +0200

    Add additional tags to Fetch to please NNE

--- lib/nne_client/fetch.rb
+++ lib/nne_client/fetch.rb
@@ -8,6 +8,9 @@ module NNEClient
     def result_set
       Request.execute(@command) do |xml|
         tag(xml, :int_1, @tdc_id, :int)
+        tag(xml, :int_2, 0, :int)
+        tag(xml, :String_3, nil, :string)
+        tag(xml, :String_4, nil, :string)
       end
     end
 
